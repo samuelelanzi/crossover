77683cd1ace635c0742111bf286449b3f0901d15
samuelelanzi <samuelelanzi00@icloud.com>
2021-05-07 21:12:39 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -140,4 +140,26 @@ int main()
 	gp4 << "set yrange [-4.5 : 6] \n";
 	gp4 << "set grid \n";
     gp4 << "plot 'FGEN_quad_2.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'VRC_quad_2.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'VRL_quad_2.txt' t 'Woofer' linecolor rgb \"#0ACE6C\"\n";
+
+    Gnuplot gp5;
+    gp5 << "set encoding utf8 \n";
+	gp5 << "set style data lines \n";
+	gp5 << "set title \"Frequency Response\" \n";
+	gp5 << "set xlabel \"Frequency [Hz]\" \n";
+	gp5 << "set autoscale x \n";
+	gp5 << "set ylabel \"Amplitude [V]\" \n";
+	gp5 << "set yrange [0.2 : 3] \n";
+	gp5 << "set grid \n";
+    gp5 << "plot 'Am1_1.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'Am2_1.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'Am3_1.txt' t 'Woofer' linecolor rgb \"#0ACE6C\"\n";
+
+    Gnuplot gp6;
+    gp6 << "set encoding utf8 \n";
+	gp6 << "set style data lines \n";
+	gp6 << "set title \"Phase Analysis\" \n";
+	gp6 << "set xlabel \"Frequency [Hz]\" \n";
+	gp6 << "set autoscale x \n";
+	gp6 << "set ylabel \"Phase [deg]\" \n";
+	gp6 << "set yrange [-60 : 80] \n";
+	gp6 << "set grid \n";
+    gp6 << "plot 'Ph1_2.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'Ph2_2.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'Ph3_2.txt' t 'Woofer' linecolor rgb \"#0ACE6C\"\n";
 }
