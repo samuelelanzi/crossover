0ed3948c6c0ea0a121e01e9127b6cf1bbfcd60f0
samuelelanzi <samuelelanzi00@icloud.com>
2021-05-10 22:44:36 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@ class Crossover
 {
 private:
     static std::vector<std::string> filename_;
+    static double RI_;
+    static double L_ , DL_;
+    static double C_ , DC_;
+    static double RL_ , DRL_, RIL_, DRIL_;
+    static double RC_ , DRC_, RIC_, DRIC_;
 
 public:
     static void add_txt(std::string const& title)
@@ -76,7 +81,7 @@ double Ph_L(double const& nu)
 
 double Ph_C(double const& nu)
 {
-    double t_C =  33.19 * 1e-9 * 1200.;
+    double t_C =  33.19e-9 * 1200.;
     return ( std::atan( 1 / (2 * M_PI * t_C * nu) ) * RADTODEG );
 }
 
@@ -142,10 +147,9 @@ int main()
     Crossover::add_txt("Ph3_2.txt");
 
     Crossover::replace_comma();
-    Crossover::print_files();
+    // Crossover::print_files();
 
     Gnuplot gp1;
-    gp1 << "set encoding utf8 \n";
     gp1 << "set style data lines \n";
     gp1 << "set title \"Crossover Filter - 1000 Hz\" \n";
     gp1 << "set xlabel \"Time [s]\" \n";
@@ -156,7 +160,6 @@ int main()
     gp1 << "plot 'FGEN_1k_1.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'VRC_1k_1.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'VRL_1k_1.txt' t 'Woofer' linecolor rgb \"#0ACE6C\"\n";
 
     Gnuplot gp2;
-    gp2 << "set encoding utf8 \n";
     gp2 << "set style data lines \n";
     gp2 << "set title \"Crossover Filter - 4000 Hz\" \n";
     gp2 << "set xlabel \"Time [s]\" \n";
@@ -167,7 +170,6 @@ int main()
     gp2 << "plot 'FGEN_4k_1.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'VRC_4k_1.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'VRL_4k_1.txt' t 'Woofer' linecolor rgb \"#0ACE6C\"\n";
 
     Gnuplot gp3;
-    gp3 << "set encoding utf8 \n";
     gp3 << "set style data lines \n";
     gp3 << "set title \"Crossover Filter - 6000 Hz\" \n";
     gp3 << "set xlabel \"Time [s]\" \n";
@@ -178,7 +180,6 @@ int main()
     gp3 << "plot 'FGEN_6k_1.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'VRC_6k_1.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'VRL_6k_1.txt' t 'Woofer' linecolor rgb \"#0ACE6C\"\n";
 
     Gnuplot gp4;
-    gp4 << "set encoding utf8 \n";
     gp4 << "set style data lines \n";
     gp4 << "set title \"Crossover Filter - Quadratic Wave - 1000 Hz\" \n";
     gp4 << "set xlabel \"Time [s]\" \n";
@@ -189,7 +190,6 @@ int main()
     gp4 << "plot 'FGEN_quad_2.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'VRC_quad_2.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'VRL_quad_2.txt' t 'Woofer' linecolor rgb \"#0ACE6C\"\n";
 
     Gnuplot gp5;
-    gp5 << "set encoding utf8 \n";
     gp5 << "set style data lines \n";
     gp5 << "set title \"Frequency Response\" \n";
     gp5 << "set xlabel \"Frequency [Hz]\" \n";
@@ -197,16 +197,31 @@ int main()
     gp5 << "set yrange [0.2 : 3] \n";
     gp5 << "set xrange [1000 : 10000] \n";
     gp5 << "set grid \n";
-    gp5 << "plot 'Am1_1.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'Am2_1.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'Am3_1.txt' t 'Woofer' linecolor rgb \"#0ACE6C\", 'VCteo.txt' t 'Tweeter Atteso', 'VLteo.txt' t 'Woofer Atteso' \n";
+    gp5 << "plot 'Am1_1.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'Am2_1.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'Am3_1.txt' t 'Woofer' linecolor rgb \"#0ACE6C\", 'VCteo.txt' t 'Tweeter Atteso' linecolor rgb \"#00599C\", 'VLteo.txt' t 'Woofer Atteso' linecolor rgb \"#108D4F\"\n";
 
     Gnuplot gp6;
-    gp6 << "set encoding utf8 \n";
     gp6 << "set style data lines \n";
     gp6 << "set title \"Phase Analysis\" \n";
     gp6 << "set xlabel \"Frequency [Hz]\" \n";
     gp6 << "set xrange [1000 : 10000] \n";
     gp6 << "set ylabel \"Phase [deg]\" \n";
-    gp6 << "set yrange [-60 : 80] \n";
+    gp6 << "set yrange [-70 : 80] \n";
     gp6 << "set grid \n";
-    gp6 << "plot 'Ph1_2.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'Ph2_2.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'Ph3_2.txt' t 'Woofer' linecolor rgb \"#0ACE6C\", 'Ph2teo.txt' t 'Tweeter Atteso', 'Ph1teo.txt' t 'Woofer Atteso' \n";
+    gp6 << "plot 'Ph1_2.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'Ph2_2.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'Ph3_2.txt' t 'Woofer' linecolor rgb \"#0ACE6C\", 'Ph2teo.txt' t 'Tweeter Atteso' linecolor rgb \"#00599C\", 'Ph1teo.txt' t 'Woofer Atteso' linecolor rgb \"#108D4F\"\n";
+
+    std::cout << "-----------------------------------------\n";
+    std::cout << "|    L    =     47.2  +/-  0.5      mH  | \n";
+    std::cout << "-----------------------------------------\n";
+    std::cout << "|    C    =     33.2  +/-  0.3      nF  | \n";
+    std::cout << "-----------------------------------------\n";
+    std::cout << "|  R_Ie   =           50           Ohm  | \n";
+    std::cout << "-----------------------------------------\n";
+    std::cout << "|   R_L   =      994  +/-   5      Ohm  | \n";
+    std::cout << "-----------------------------------------\n";
+    std::cout << "|   R_C   =      993  +/-   5      Ohm  | \n";
+    std::cout << "-----------------------------------------\n";
+    std::cout << "|   R_IL  =    198.8  +/-  0.5     Ohm  | \n";
+    std::cout << "-----------------------------------------\n";
+    std::cout << "|  R_IC   =      201  +/-   1      Ohm  | \n";
+    std::cout << "-----------------------------------------\n";
 }
