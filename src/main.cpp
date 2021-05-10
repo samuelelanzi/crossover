e7bfccfb15f348f6345cf5476a4a6b253e734437
samuelelanzi <samuelelanzi00@icloud.com>
2021-05-10 21:02:20 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "gnuplot-iostream.h"
 
+constexpr double RADTODEG = 180. / M_PI;
+
 class Crossover
 {
 private:
@@ -50,8 +52,53 @@ public:
 
 std::vector<std::string> Crossover::filename_ {};
 
+double V_L(double const& nu)
+{
+    double r = 1000. / 1200.;
+    double V = 2.5;
+    double t_L = 0.04719 / 1200.;
+    return (r * V) / std::sqrt(1 + std::pow( 2 * M_PI * nu * t_L, 2 ) );
+}
+
+double V_C(double const& nu)
+{
+    double r = 1000. / 1200.;
+    double V = 2.5;
+    double t_C =  33.19 * 1e-9 * 1200.;
+    return (r * V) / std::sqrt( 1 + (1 / std::pow( 2 * M_PI * nu * t_C, 2 ) ) );
+}
+
+double Ph_L(double const& nu)
+{
+    double t_L = 0.04719 / 1200.;
+    return ( - std::atan( 2 * M_PI * t_L * nu * 1 ) * RADTODEG );
+}
+
+double Ph_C(double const& nu)
+{
+    double t_C =  33.19 * 1e-9 * 1200.;
+    return ( std::atan( 1 / (2 * M_PI * t_C * nu) ) * RADTODEG );
+}
+
 int main()
 {
+    std::ofstream fout1 {"VLteo.txt"};
+    std::ofstream fout2 {"VCteo.txt"};
+    std::ofstream fout3 {"Ph1teo.txt"};
+    std::ofstream fout4 {"Ph2teo.txt"};
+
+    for (int i{}; i != 1e5; ++i) {
+        fout1 << static_cast<double>(i) / 10. << ' ' << V_L(static_cast<double>(i) / 10.) << '\n';
+        fout2 << static_cast<double>(i) / 10. << ' ' << V_C(static_cast<double>(i) / 10.) << '\n';
+        fout3 << static_cast<double>(i) / 10. << ' ' << Ph_L(static_cast<double>(i) / 10.) << '\n';
+        fout4 << static_cast<double>(i) / 10. << ' ' << Ph_C(static_cast<double>(i) / 10.) << '\n';
+    }
+    
+    fout1.close();
+    fout2.close();
+    fout3.close();
+    fout4.close();
+
     Crossover::add_txt("FGEN_1k_1.txt");
     Crossover::add_txt("VRC_1k_1.txt");
     Crossover::add_txt("VRL_1k_1.txt");
@@ -146,20 +193,20 @@ int main()
     gp5 << "set style data lines \n";
     gp5 << "set title \"Frequency Response\" \n";
     gp5 << "set xlabel \"Frequency [Hz]\" \n";
-    gp5 << "set autoscale x \n";
     gp5 << "set ylabel \"Amplitude [V]\" \n";
     gp5 << "set yrange [0.2 : 3] \n";
+    gp5 << "set xrange [1000 : 10000] \n";
     gp5 << "set grid \n";
-    gp5 << "plot 'Am1_1.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'Am2_1.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'Am3_1.txt' t 'Woofer' linecolor rgb \"#0ACE6C\"\n";
+    gp5 << "plot 'Am1_1.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'Am2_1.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'Am3_1.txt' t 'Woofer' linecolor rgb \"#0ACE6C\", 'VCteo.txt' t 'Tweeter Atteso', 'VLteo.txt' t 'Woofer Atteso' \n";
 
     Gnuplot gp6;
     gp6 << "set encoding utf8 \n";
     gp6 << "set style data lines \n";
     gp6 << "set title \"Phase Analysis\" \n";
     gp6 << "set xlabel \"Frequency [Hz]\" \n";
-    gp6 << "set autoscale x \n";
+    gp6 << "set xrange [1000 : 10000] \n";
     gp6 << "set ylabel \"Phase [deg]\" \n";
     gp6 << "set yrange [-60 : 80] \n";
     gp6 << "set grid \n";
-    gp6 << "plot 'Ph1_2.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'Ph2_2.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'Ph3_2.txt' t 'Woofer' linecolor rgb \"#0ACE6C\"\n";
+    gp6 << "plot 'Ph1_2.txt' t 'FGEN' linecolor rgb \"#FF6C6C\", 'Ph2_2.txt' t 'Tweeter' linecolor rgb \"#189CFF\", 'Ph3_2.txt' t 'Woofer' linecolor rgb \"#0ACE6C\", 'Ph2teo.txt' t 'Tweeter Atteso', 'Ph1teo.txt' t 'Woofer Atteso' \n";
 }
